7152f419284644b5fca44ef5af84b4c7f332b2a4
tsalmon <th_s@hotmail.fr>
2014-01-31 21:26:23 +0100

    SYS 2013-2014 TP1 256, --error() => dysfonctionnement programme

--- M1/Systeme/TP/2013-2014/TP1/ex1.c
+++ M1/Systeme/TP/2013-2014/TP1/ex1.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
 int error()
@@ -33,53 +34,43 @@ int error()
 }
 int main()
 {
-  size_t count = 10;
-  char c[10];
-  char buffer[20];
+  size_t count = 128;
+  char c[128];
+  char buffer[128];
   ssize_t i;
   int j = 0, k = 0, nb_chars = 0;
   
-  while((i = read(0, c, count)) > 0)
-    {
-      if(error()){
-	return 1;
-      }
-      if(c[0] == 'e' && c[1] == 'x' && c[2] == 'i' && c[3] == 't'){
-	return 0;
-      }
-      
-      nb_chars += i;
-      
-      for(j = 0; j < 20; j++)
-	{
-	  buffer[j] = 0;
-	}
-
-      buffer[0] = (k == 1 && c[0] == '*') ? '^' : c[0];
-      for(j = 0; k < i; k++, j++)
-	{
-	  if((k < i-1) && (c[k] == c[k+1] && c[k] == '*'))
-	    {
-	      buffer[j] = '^';
-	    }
-	  else if(c[k] == '^')
-	    {
-	      buffer[j++] = '*';
-	      buffer[j] = '*';
-	    }
-	  else
-	    {
-	      buffer[j] = c[k];
-	    }
-	}
-      k = (c[9] == '*') ? 1 : 0;
-
-      if(nb_chars > 80)
-	{
-	  write(1, "\n", 1);
-	  nb_chars = 0;
-	}
-      write(1, buffer, j);
+  while((i = read(0, c, count)) > 0){
+    if(c[0] == 'e' && c[1] == 'x' && c[2] == 'i' && c[3] == 't'){
+      return 0;
     }
+    nb_chars += i;
+    
+    for(j = 0; j < 256; j++){
+      buffer[j] = 0;
+    }
+    
+    buffer[0] = (k == 1 && c[0] == '*') ? '^' : c[0];
+    for(j = 0; k < i; k++, j++){
+      if((k < i-1) && (c[k] == c[k+1] && c[k] == '*')){
+	buffer[j] = '^';
+      } else if(c[k] == '^'){
+	buffer[j++] = '*';
+	buffer[j] = '*';
+      } else {
+	buffer[j] = c[k];
+      }
+    }
+    k = (c[9] == '*') ? 1 : 0;
+    
+    if(nb_chars > 80){
+      write(1, "\n", 1);
+      nb_chars = 0;
+    }
+    write(1, buffer, j);
+    for(j = 0; j < 256; j++){
+      c[j] = 0;
+    }
+  }
   return 0;
 }
