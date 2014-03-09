0d086bbf84a7f4bf3c4c2eae6d9f6f932bedaff2
Baptiste Fontaine <batifon@yahoo.fr>
2014-03-09 20:28:33 +0100

    starting a clean M1/Systeme directory

--- M1/Systeme/TP/2013-2014/TP1/ex2.h
+++
@@ -1,7 +0,0 @@
-#ifndef EX2_H
-#define EX2_H
-#define DIR_BASE(str) (!strcmp("..", str) || !strcmp(".", str))
-void parcourir_rep(char *directory_name);
-void confirme_suppr(char *filename);
-void take_file(char *filename);
-#endif
--- M1/Systeme/TP/2013-2014/tp5ex1.sh
+++
@@ -1,3 +0,0 @@
-display=`man ls | grep -n "display[^a-zA-Z0-9]\|display$" | wc -l`
-list=`man ls | grep -n "list[^a-zA-Z0-9]\|list$" | wc -l`
-echo "display=$display, list=$list"
